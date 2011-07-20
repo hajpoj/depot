3a2bcf08f7dca35444f1388ac32ebfaa2db63db9
Hajime Furukawa <hajpoj@gmail.com>
2011-07-20 00:39:49 -0700

    changes from chapter 9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::Base
+  helper :all
   protect_from_forgery
+
+  private
+
+    def current_cart
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+
+    def increment_count
+      if session[:counter].nil?
+        session[:counter] = 0
+      end
+      session[:counter] += 1
+    end
+
+    def zero_count
+      session[:counter] = 0
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def show_count
+    if session[:counter] >= 5
+      "Visited: " + pluralize(session[:counter], "time")
+    else
+      ""
+    end
+  end
 end
